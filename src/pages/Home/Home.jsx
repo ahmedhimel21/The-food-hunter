2601ebb1107eb655d4fdf39b0e9d47d9c27f6673
ahmedhimel21 <ahmedhimel000@gmail.com>
2023-05-03 19:09:18 +0600

    see more button implemented

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Lottie from "lottie-react";
 import carousel from "../../assets/carousel.json";
 import { useLoaderData } from "react-router-dom";
@@ -6,6 +6,11 @@ import SingleChefCards from "../singleChefCards/singleChefCards";
 
 const Home = () => {
   const chefData = useLoaderData();
+  const [showAll, setShowAll] = useState(false);
+
+  const handleShowAll = () => {
+    setShowAll(!showAll);
+  };
   return (
     <>
       <div
@@ -27,13 +32,20 @@ const Home = () => {
         </div>
       </div>
       <h1 className="text-3xl font-bold text-center">Our Chef</h1>
-  <div className="grid grid-cols-1 md:gird-cols-2 lg:grid-cols-2 gap-5 mx-5 my-7">
-        {chefData.map((singleData) => (
+      <div className="grid grid-cols-1 md:gird-cols-2 lg:grid-cols-2 gap-5 mx-5 my-7">
+        {chefData.slice(0, showAll ? 6 : 4).map((singleData) => (
           <SingleChefCards
             key={singleData.id}
             singleData={singleData}
           ></SingleChefCards>
         ))}
+      </div>
+      <div className="text-center mb-4">
+        {!showAll && (
+          <button onClick={handleShowAll} className="btn btn-secondary">
+            See More
+          </button>
+        )}
       </div>
     </>
   );
